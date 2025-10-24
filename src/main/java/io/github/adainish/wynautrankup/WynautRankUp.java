a5c7f491893531a7c65af9ed78d86ae119df86f5
Adenydd Catherine Stuut <wingletdev@gmail.com>
2025-10-24 19:50:21 +0200

    - Licensing

--- src/main/java/io/github/adainish/wynautrankup/WynautRankUp.java
+++ src/main/java/io/github/adainish/wynautrankup/WynautRankUp.java
@@ -1,3 +1,19 @@
+/*
+ * Program: WynautRankup - Add a competitive ranked system to Cobblemon
+ * Copyright (C) <2025> <Nicole "Adenydd" Catherine Stuut>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * See the `LICENSE` file in the project root or <https://www.gnu.org/licenses/>.
+ */
 package io.github.adainish.wynautrankup;
 
 import com.cobblemon.mod.common.api.Priority;
